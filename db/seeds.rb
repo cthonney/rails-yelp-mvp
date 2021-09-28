cccacb730dba4dbbcdc95676de8395507279790f
cedric thonney <c.thonney@gmail.com>
2021-09-28 21:32:20 +0400

    presque fini

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+
+5.times do |number|
+
+  Restaurant.create!(
+    name: Faker::Creature::Animal.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: [ "chinese", "italian", "japanese", "french", "belgian" ].sample
+  )
+end
